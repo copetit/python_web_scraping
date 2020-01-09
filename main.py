3fe994788c3ad9e4a39fb71fbd49fad0a00bc0e5
hanseola <copetit412@gmail.com>
2020-01-09 22:09:20 +0900

    tuple dict

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 days = ["mon","tue","wed","thur","fri"]
 
+
 print("mon" in days)
 days.append("sun")
 print(days)
 days.reverse()
 print(days)
+
+days = ("mon","tue","wed","thur","fri")
+
+print(days)
+
+# dic
+
+nico = {
+  "name" : "nico",
+  "age" : 29,
+  "korean" : True,
+  "fav_food" : ["kimchi", "Sashimi"]
+}
+nico["handsome"] = True
+print(nico)
