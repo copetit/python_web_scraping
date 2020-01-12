653409e443c1754d3994cb60f7ea633dee6c2d5f
hanseola <copetit412@gmail.com>
2020-01-12 22:01:04 +0900

    basic video done

--- main.py
+++ main.py
@@ -1,28 +1,6 @@
-days = ["mon","tue","wed","thur","fri"]
+from math import ceil, fsum
+import cal
+print(ceil(1.2))
+print(fsum([1, 2, 3, 4, 5, 6]))
 
-
-print("mon" in days)
-days.append("sun")
-print(days)
-days.reverse()
-print(days)
-
-days = ("mon","tue","wed","thur","fri")
-
-print(days)
-
-# dic
-
-nico = {
-  "name" : "nico",
-  "age" : 29,
-  "korean" : True,
-  "fav_food" : ["kimchi", "Sashimi"]
-}
-nico["handsome"] = True
-print(nico)
-# 1.3
-print(len("hoge"))
-age = "18"
-age_n = int(age)
-print(type(age_n))
+print(cal.plus(1,2))
