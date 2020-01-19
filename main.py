d99341df1615aa11d92a1b0c996949a1218ccd91
hanseola <copetit412@gmail.com>
2020-01-19 20:05:04 +0900

    URL抽出

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-import requests
-from bs4 import BeautifulSoup
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
-indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
-indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
-# https://stackoverflow.com/jobs?q=python
+last_indeed_page = extract_indeed_pages()
 
-pagination = indeed_soup.find("div", {"class": "pagination"})
-# list
-pages = pagination.find_all('a')
-
-spans = []
-for page in pages: 
-  spans.append(page.find("span"))
-spans = spans[:-1]
+extract_indeed_jobs(last_indeed_page)
