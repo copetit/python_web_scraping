0157d85ba79f03bd91f0baea2c61e5155804af32
hanseola <copetit412@gmail.com>
2020-01-20 20:56:08 +0900

    get title

--- indeed.py
+++ indeed.py
@@ -21,7 +21,13 @@ def extract_indeed_pages():
 
 def extract_indeed_jobs(last_pages):
     jobs = []
-    for page in range(last_pages):
-        result = requests.get(f"{URL}&start={page * LIMIT}")
-        print(result.status_code)
+    #for page in range(last_pages):
+    result = requests.get(f"{URL}&start={0 * LIMIT}")
+    soup = BeautifulSoup(result.text, "html.parser")
+    results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+    print(results)
+    for result in results:
+        title = result.find("div",{"class": "title"}).find("a")["title"]
+        print(title)
+
     return jobs
