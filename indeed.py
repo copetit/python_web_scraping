c2e7176bd9f289b1ff3c057525164f9406962fed
hanseola <copetit412@gmail.com>
2020-01-23 22:14:15 +0900

    cleaning func

--- indeed.py
+++ indeed.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 LIMIT = 50
 URL = f"https://www.indeed.com/jobs?q=python&limit={LIMIT}"
 
-def extract_indeed_pages():  
+def get_last_page():
   result = requests.get(URL)
   soup = BeautifulSoup(result.text, "html.parser")
 
@@ -35,9 +35,9 @@ def extract_job(html):
     job_id = html["data-jk"]
     return {'title': title, 'company': company, 'location': location, 'link': f"https://www.indeed.com/viewjob?jk={job_id}"}
 
-def extract_indeed_jobs(last_pages):
+def extract_jobs(last_page):
     jobs = []
-    for page in range(last_pages):
+    for page in range(last_page):
         print(f"Scrapping page {page}")
         result = requests.get(f"{URL}&start={page * LIMIT}")
         soup = BeautifulSoup(result.text, "html.parser")
@@ -45,4 +45,9 @@ def extract_indeed_jobs(last_pages):
         for result in results:
             job = extract_job(result)
             jobs.append(job)
+    return jobs
+
+def get_jobs():
+    last_page = get_last_page()
+    jobs = extract_jobs(last_page)
     return jobs
