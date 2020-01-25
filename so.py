602e1c9a00b161560311a0dd9012140a22112981
hanseola <copetit412@gmail.com>
2020-01-25 22:34:59 +0900

    get job-id

--- so.py
+++ so.py
@@ -7,10 +7,21 @@ URL = f"https://stackoverflow.com/jobs?q=python&sort=i"
 def get_last_page():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, "html.parser")
-    print(soup)
     pages = soup.find("div", {"class":"s-pagination"}).find_all("a")
-    print(pages)
+    last_pages = pages[-2].get_text(strip=True)
+    return int(last_pages)
+
+def extract_jobs(last_page):
+    jobs = []
+    for page in range(last_page):
+        result = requests.get(f"{URL}&pg={page+1}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div",{"class" : "-job"})
+        for result in results:
+            print(result["data-jobid"])
+
 
 def get_jobs():
     last_page = get_last_page()
-    return [] 
+    jobs = extract_jobs(last_page)
+    return last_page
