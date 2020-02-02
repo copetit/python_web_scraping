e302a1520636ef72f9cd43662953f347ed45fc9e
hanseola <copetit412@gmail.com>
2020-02-02 18:48:42 +0900

    start make csvfile

--- save.py
+++ save.py
@@ -1,8 +1,10 @@
 import csv
 
-def save_to_file():
+def save_to_file(jobs):
     file = open("jobs.csv", mode = "w")
     print(file)
     writer = csv.writer(file)
     writer.writerow(["title","company","locatioin","link"])
+    for job in jobs:
+        writer.writerow(list(job.values()))
     return
