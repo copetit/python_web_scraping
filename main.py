94f3720093b1cdead652a748912b5cefeb5309f7
hanseola <copetit412@gmail.com>
2020-02-11 21:11:17 +0900

    完成

--- main.py
+++ main.py
@@ -5,6 +5,5 @@ from save import save_to_file
 indeed_jobs = get_indeed_jobs()
 so_jobs = get_so_jobs()
 
-jobs = indeed_jobs
+jobs = so_jobs + indeed_jobs
 save_to_file(jobs)
-
--- save.py
+++ save.py
@@ -2,7 +2,6 @@ import csv
 
 def save_to_file(jobs):
     file = open("jobs.csv", mode = "w")
-    print(file)
     writer = csv.writer(file)
     writer.writerow(["title","company","locatioin","link"])
     for job in jobs:
